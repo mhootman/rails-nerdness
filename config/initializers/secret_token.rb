b02795029721a905af381de21a89f5e4624d5bc0
Matt Hootman <matt.hootman@cerner.com>
2013-01-15 20:03:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarchNerdness::Application.config.secret_token = '44e4ebb5471d25758fe6e3723bb51e12940c4d82a7c504ac15c27dcd61b38329911f7558e105c7fe8a79f666dcf01304468fe12bd8cfdb3499e4aec62f3f3e8a'
